5db6398baba90fa918767b2c215c9220ad928df4
johnhansarickWallaroo <john.hummel@wallaroo.ai>
2023-07-25 16:40:06 -0600

    forecast testing

--- forecast/pipeline_multiple_replicas_forecast_tutorial/forecast_standard.py
+++ forecast/pipeline_multiple_replicas_forecast_tutorial/forecast_standard.py
@@ -10,29 +10,26 @@ def _fit_model(dataframe):
     return model
 
 
-def wallaroo_json(data):
-    obj = json.loads(data)
-    evaluation_frame = pd.DataFrame.from_dict(obj)
-
-    nforecast = 7
-    model = _fit_model(evaluation_frame)
-
-    forecast =  model.forecast(steps=nforecast).round().to_numpy()
-    forecast = forecast.astype(int)
-
-    return {"forecast": forecast.tolist()}
-
-# assuming a dataframe was inserted
 # def wallaroo_json(data):
-#     # obj = json.loads(data)
-#     # evaluation_frame = pd.DataFrame.from_dict(obj)
-#     evaluation_frame = data
+#     obj = json.loads(data)
+#     evaluation_frame = pd.DataFrame.from_dict(obj)
+
 #     nforecast = 7
 #     model = _fit_model(evaluation_frame)
 
 #     forecast =  model.forecast(steps=nforecast).round().to_numpy()
 #     forecast = forecast.astype(int)
 
-    
+#     return {"forecast": forecast.tolist()}
 
-#     return pd.DataFrame({"forecast":[forecast]})
+# assuming a dataframe was inserted
+def wallaroo_json(data: pd.DataFrame):
+    data = pd.DataFrame({'cnt': data['cnt'][0]})
+    print(data)
+    nforecast = 7
+    model = _fit_model(data)
+
+    forecast =  model.forecast(steps=nforecast).round().to_numpy()
+    forecast = forecast.astype(int)
+
+    return pd.DataFrame({"forecast":[forecast]})
