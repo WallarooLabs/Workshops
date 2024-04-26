345a3161f576d3d647274884698b7b0b2eeda560
johnhansarickWallaroo <john.hummel@wallaroo.ai>
2024-04-26 08:15:59 -0600

    workshop updates for 2024.1

--- Linear Regression/Real Estate/orchestration/real-estate-orchestration/main.py
+++ Linear Regression/Real Estate/orchestration/real-estate-orchestration/main.py
@@ -17,37 +17,16 @@ if "pipeline_name" in arguments:
 else:
     pipeline_name="bikedaypipe"
 
-def get_workspace(name):
-    workspace = None
-    for ws in wl.list_workspaces():
-        if ws.name() == name:
-            workspace= ws
-    return workspace
-
-def get_pipeline(pipeline_name, workspace):
-    plist = workspace.pipelines()
-    pipeline = [p for p in plist if p.name() == pipeline_name]
-    if len(pipeline) <= 0:
-        raise KeyError(f"Pipeline {pipeline_name} not found in this workspace")
-        return None
-    return pipeline[0]
-
-# pull a single datum from a data frame 
-# and convert it to the format the model expects
-def get_singleton(df, i):
-    singleton = df.iloc[i,:].to_numpy().tolist()
-    sdict = {'tensor': [singleton]}
-    return pd.DataFrame.from_dict(sdict)
 
 print(f"Workspace: {workspace_name}")
-workspace = get_workspace(workspace_name)
+workspace = wl.get_workspace(workspace_name)
 
 wl.set_current_workspace(workspace)
 print(workspace)
 
 # the pipeline is assumed to be deployed
 print(f"Pipeline: {pipeline_name}")
-pipeline = get_pipeline(pipeline_name, workspace)
+pipeline = wl.get_pipeline(pipeline_name, workspace)
 print(pipeline)
 
 print(pipeline.status())
