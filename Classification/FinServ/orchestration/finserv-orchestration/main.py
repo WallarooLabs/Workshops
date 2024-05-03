b908c7096a3e57a6f0ae7929ba48c0347a2ece08
johnhansarickWallaroo <john.hummel@wallaroo.ai>
2024-05-03 09:09:54 -0600

    updated various workshops

---
+++ Classification/FinServ/orchestration/finserv-orchestration/main.py
@@ -0,0 +1,35 @@
+import wallaroo
+from wallaroo.object import EntityNotFoundError
+import pandas as pd
+
+wl = wallaroo.Client()
+
+# get the arguments
+arguments = wl.task_args()
+
+if "workspace_name" in arguments:
+    workspace_name = arguments['workspace_name']
+else:
+    workspace_name="finserv-workshop"
+
+if "pipeline_name" in arguments:
+    pipeline_name = arguments['pipeline_name']
+else:
+    pipeline_name="finserv-pipe"
+
+
+print(f"Workspace: {workspace_name}")
+workspace = wl.get_workspace(workspace_name)
+
+wl.set_current_workspace(workspace)
+print(workspace)
+
+# the pipeline is assumed to be deployed
+print(f"Pipeline: {pipeline_name}")
+pipeline = wl.get_pipeline(pipeline_name)
+print(pipeline)
+
+print(pipeline.status())
+
+inference_result = pipeline.infer_from_file('./data/cc_data_1k.df.json')
+print(inference_result)
